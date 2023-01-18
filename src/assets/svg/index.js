362a528ffd89b6367816905a2917043de3ec941c
alisarfaraj <sarfaraj.ali@digimonk.in>
2023-01-18 12:37:06 +0530

    add job-seeker profile page

--- src/assets/svg/index.js
+++ src/assets/svg/index.js
@@ -11,6 +11,12 @@ import { ReactComponent as EyeOff } from "./eyeOff.svg";
 import { ReactComponent as EyeOpen } from "./eyeOpen.svg";
 import { ReactComponent as UserIcon } from "./user.svg";
 import { ReactComponent as DashboardIcon } from "./dashboard.svg";
+import { ReactComponent as ArrowUpIcon } from "./arrowupIcon.svg";
+import { ReactComponent as CheckBoxIcon } from "./checkBoxIcon.svg";
+import { ReactComponent as CheckIcon } from "./checkIcon.svg";
+import { ReactComponent as OneIcon } from "./oneIcon.svg";
+import { ReactComponent as StartIcon } from "./startIcon.svg";
+import { ReactComponent as UncheckICon } from "./uncheckICon.svg";
 export const SVG = {
   KoorLogo,
   SearchIcon,
@@ -25,4 +31,10 @@ export const SVG = {
   EyeOpen,
   UserIcon,
   DashboardIcon,
+  ArrowUpIcon,
+  CheckBoxIcon,
+  CheckIcon,
+  OneIcon,
+  StartIcon,
+  UncheckICon,
 };
--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,5 +1,7 @@
 import FilledButtonComponent from "./filledButton";
 import OutlinedButtonComponent from "./outlinedButton";
+import CbuttonButtonComponent from "./Cbutton";
 
 export const FilledButton = FilledButtonComponent;
 export const OutlinedButton = OutlinedButtonComponent;
+export const Cbutton = CbuttonButtonComponent;
