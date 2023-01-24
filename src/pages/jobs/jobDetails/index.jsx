1269b61513a050770db1169c4e13fa616b57ca06
Saral Shrivastava <shrivastavaa278@gmail.com>
2023-01-24 17:25:14 +0530

    update feedback

--- src/pages/jobs/jobDetails/index.jsx
+++ src/pages/jobs/jobDetails/index.jsx
@@ -128,7 +128,7 @@ const JobDetails = () => {
                   </div>
                 </div>
                 <div className={`${styles.jobpostbtn}`}>
-                  <Link to="../jobs/applyJob/:jobId">
+                  <Link to="../job/apply/:jobId">
                     <Searchbutton
                       text="Apply for this job"
                       className={`${styles.enablebtn}`}
