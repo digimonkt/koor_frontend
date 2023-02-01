b2c76e0663307a1c6d09de42d8f34e17064b969b
alisarfaraj <sarfaraj.ali@digimonk.in>
2023-02-01 12:42:15 +0530

    add tender, message page and post tender page

--- src/pages/employer/index.js
+++ src/pages/employer/index.js
@@ -1,8 +1,10 @@
 import ManageJobsComponent from "./manageJobs";
 import MyProfileComponent from "./myProfile";
 import DashboardComponent from "./dashboard";
+import ManageTendersComponent from "./manageTenders";
 import "./employer.css";
 
 export const MyProfile = MyProfileComponent;
 export const ManageJobs = ManageJobsComponent;
 export const Dashboard = DashboardComponent;
+export const ManageTenders = ManageTendersComponent;
