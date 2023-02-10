cd663d501f44514c22b15e4d8550a3d1c5e22190
Raymond9131 <fullstackdev@superiorcsgroup.com>
2023-02-10 16:30:56 +0530

    home footer

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,19 @@
 import { Box, Container } from "@mui/material";
-import React from "react";
+import { setIstHomePage } from "@redux/slice/user";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 const Home = () => {
+  // redux dispatch
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(setIstHomePage(true));
+
+    return () => {
+      dispatch(setIstHomePage(false));
+    };
+  }, []);
+
   return (
     <Box sx={{ marginTop: "68px" }}>
       <Container>Home</Container>
