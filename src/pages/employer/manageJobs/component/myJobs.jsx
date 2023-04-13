72d415591df39712997dc2b4975b8487fe25b9e2
Sandeep Singh Sikarwar <29565705+TheSikarwar@users.noreply.github.com>
2023-04-13 17:20:35 +0530

    search job in emoloyer

--- src/pages/employer/manageJobs/component/myJobs.jsx
+++ src/pages/employer/manageJobs/component/myJobs.jsx
@@ -10,8 +10,10 @@ import { setTotalCreatedJobs } from "@redux/slice/employer";
 function MyJobs() {
   const dispatch = useDispatch();
   const [jobs, setJobs] = useState([]);
+  const [search, setSearch] = useState();
+  const [isSearching, setIsSearching] = useState(false);
   const getAllJobs = useCallback(async () => {
-    const res = await getEmployerJobsAPI();
+    const res = await getEmployerJobsAPI({ search });
     if (res.remote === "success") {
       setJobs(res.data.results);
       dispatch(setTotalCreatedJobs(res.data.totalCount || res.data.count));
@@ -19,15 +21,25 @@ function MyJobs() {
       console.log(res);
     }
   }, []);
+
   useEffect(() => {
     getAllJobs();
-  }, []);
+  }, [isSearching]);
   return (
     <div className="py-3">
       <div className="mb-3">
         <Stack direction="row" spacing={0} className="searchjob-box">
-          <input className="jobsearch" placeholder="Search your jobs" />
-          <button className="jobt-btn-search">{<SVG.SearchIcon />}</button>
+          <input
+            className="jobsearch"
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder="Search your jobs"
+          />
+          <button
+            className="jobt-btn-search"
+            onClick={() => setIsSearching(!isSearching)}
+          >
+            {<SVG.SearchIcon />}
+          </button>
         </Stack>
       </div>
       {jobs.map((job) => {
