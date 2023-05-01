f0de775f8c21d7a80c16c65d6468d5c7e557b838
Saral Shrivastava <shrivastavaa278@gmail.com>
2023-05-01 16:53:20 +0530

    improved lazy loading and sudden page refresh

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,6 +4,8 @@ import ChoiceReducer from "./slice/choices";
 import EmployerReducer from "./slice/employer";
 import ToastReducer from "./slice/toast";
 import JobReducer from "./slice/search";
+import logger from "redux-logger";
+const isProd = process.env.NODE_ENV === "production";
 export const store = configureStore({
   reducer: {
     auth: AuthReducer,
@@ -11,5 +13,13 @@ export const store = configureStore({
     employer: EmployerReducer,
     toast: ToastReducer,
     search: JobReducer,
+  },
+  devTools: !isProd,
+  middleware: (getDefaultMiddleware) => {
+    if (!isProd) {
+      return getDefaultMiddleware().concat(logger);
+    } else {
+      return getDefaultMiddleware();
+    }
   },
 });
