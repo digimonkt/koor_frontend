775ae3fbaf9fbe79efa62cd4082e763b913ee7f4
Saral Shrivastava <shrivastavaa278@gmail.com>
2023-05-11 16:29:46 +0530

    update tender filter

--- src/api/transform/vendor.js
+++ src/api/transform/vendor.js
@@ -1,18 +1,16 @@
 export const transformTenderSavedFilter = (data) => {
   return {
     id: data.id,
-    jobCategories: (data.tender_category || []).map(
-      (categoryData) => categoryData.id
-    ),
-    country: data.country?.id ? data.country : null,
-    city: data.city?.id ? data.city : null,
-    opportunityType: data.opportunity_type,
-    tag: data.tag,
-    isNotification: data.is_notification,
     title: data.title,
+    country: data.country?.id ? data.country : "",
+    city: data.city?.id ? data.city : "",
+    opportunityType: (data.opportunity_type || []).map((data) => data.id),
+    sector: (data.sector || []).map((sector) => sector.id),
+    tag: (data.tag || []).map((tag) => tag.id),
+    tenderCategories: (data.tender_category || []).map((cate) => cate.id),
+    isNotification: data.is_notification,
     deadline: data.deadline,
     budgetMin: data.budget_min,
     budgetMax: data.budget_max,
-    sector: (data.sector || []).map((sector) => sector.title),
   };
 };
--- src/pages/search/advanceFilter/index.jsx
+++ src/pages/search/advanceFilter/index.jsx
@@ -147,6 +147,7 @@ function AdvanceFilter({ searchType }) {
   };
 
   const handleSelectFilter = async (filter) => {
+    console.log({ filter });
     setSelectedFilter(filter.id);
     formik.setFieldValue("id", filter.id);
     formik.setFieldValue("jobCategories", filter.jobCategories);
@@ -159,11 +160,12 @@ function AdvanceFilter({ searchType }) {
     formik.setFieldValue("salaryMin", filter.salaryMin);
     formik.setFieldValue("salaryMax", filter.salaryMax);
     // tenders
+    formik.setFieldValue("opportunityType", filter.opportunityType);
+    formik.setFieldValue("sector", filter.sector);
+    formik.setFieldValue("tag", filter.tag);
+    formik.setFieldValue("tenderCategories", filter.tenderCategories);
     formik.setFieldValue("budgetMin", filter.budgetMin);
     formik.setFieldValue("budgetMax", filter.budgetMax);
-    formik.setFieldValue("sector", filter.sector);
-    formik.setFieldValue("tenderCategory", filter.tenderCategory);
-    formik.setFieldValue("tag", filter.tag);
     formik.setFieldValue("deadline", filter.deadline);
     const payload = {
       country: filter.country?.title || "",
@@ -294,9 +296,9 @@ function AdvanceFilter({ searchType }) {
     const data = {
       title,
       country: rawData.country,
-      category: rawData.jobCategories,
+      tender_category: rawData.tenderCategories,
       sector: rawData.sector,
-      opportunity_type: rawData.isPartTime,
+      opportunity_type: rawData.opportunityType,
       tag: rawData.tag,
       budget_min: rawData.budgetMin,
       budget_max: rawData.budgetMax,
