46006e10c87391aeab855524e447d01419353d56
Sandeep Singh Sikarwar <29565705+TheSikarwar@users.noreply.github.com>
2023-08-29 10:54:50 +0530

    fix facebook login social id

--- src/pages/auth/layout.jsx
+++ src/pages/auth/layout.jsx
@@ -128,7 +128,7 @@ function AuthLayout({
           role,
           name: res.data.displayName,
           display_image: res.data.photoURL,
-          social_login_id: res.data.localId,
+          social_login_id: res.data.uid,
           source: "facebook",
         };
         const result = await SocialLoginAPI(payload);
