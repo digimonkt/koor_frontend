7be2801fefde0c2a1d05c3f841d8a73cb93dc577
Sandeep Singh Sikarwar <29565705+TheSikarwar@users.noreply.github.com>
2023-09-12 18:28:14 +0530

    post tender date issue done

--- src/pages/tenders/applyForTender/validator.js
+++ src/pages/tenders/applyForTender/validator.js
@@ -1,6 +1,9 @@
 import * as Yup from "yup";
 
 export const applyTenderValidationSchema = Yup.object().shape({
-  shortLetter: Yup.string(),
-  attachments: Yup.array(),
+  shortLetter: Yup.string()
+    .required("Please enter short-letter")
+    // .trim() // Trim leading and trailing white spaces,
+    .matches(/^\s*\S+(\s+\S+)*\s*$/, "Short letter cannot contain only white spaces"), // Custom rule to disallow only white spaces
+  attachments: Yup.array().test("attachments", "Please add attachment", (value) => value.length > 0),
 });
