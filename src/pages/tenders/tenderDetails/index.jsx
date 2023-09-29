42676337283865526ae4a189150ff760a5bc45fc
Anjali <2005anjalirajput.er@gmail.com>
2023-09-29 15:55:27 +0530

    koor points resolve

--- src/pages/tenders/tenderDetails/index.jsx
+++ src/pages/tenders/tenderDetails/index.jsx
@@ -306,7 +306,7 @@ function TenderDetailsComponent() {
                           ? details.isEditable
                             ? "Edit"
                             : "Applied"
-                          : "Apply for this Tender"
+                          : "Apply for th Tender"
                       }
                       className={`${styles.enablebtn}`}
                       disabled={details.isApplied && !details.isEditable}
