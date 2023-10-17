759786b82be6aa1b7c708ea1d92738830e901818
Sandeep Singh Sikarwar <29565705+TheSikarwar@users.noreply.github.com>
2023-10-17 11:44:40 +0530

    unhide validate env code

--- src/utils/validateEnv.js
+++ src/utils/validateEnv.js
@@ -1,8 +1,8 @@
-// import { cleanEnv, str } from "envalid";
-// const env = cleanEnv(process.env, {
-//   REACT_APP_BACKEND_URL: str(),
-//   REACT_APP_WS_BACKEND_URL: str(),
-//   REACT_APP_GOOGLE_API_KEY: str({ default: "" }),
-// });
+import { cleanEnv, str } from "envalid";
+const env = cleanEnv(process.env, {
+  REACT_APP_BACKEND_URL: str(),
+  REACT_APP_WS_BACKEND_URL: str(),
+  REACT_APP_GOOGLE_API_KEY: str({ default: "" }),
+});
 
-// export default env;
+export default env;
