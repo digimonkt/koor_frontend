d6f68206bcd5ae675d5579028e98b5d3977a8493
Sandeep Singh Sikarwar <29565705+TheSikarwar@users.noreply.github.com>
2023-11-06 15:40:49 +0530

    hide tab in notification when no notification, job status color change

--- src/utils/generateColor.js
+++ src/utils/generateColor.js
@@ -1,7 +1,16 @@
 export const getColorByRemainingDays = (days) => {
-  const limit = 50;
-  const value = days > limit ? 0 : 1 - days / limit;
-  // value from 0 to 1
-  const hue = ((1 - value) * 120).toString(10);
-  return `hsl(${hue}, 100%, 50%)`;
+  // const limit = 50;
+  // const value = days > limit ? 0 : 1 - days / limit;
+  // // value from 0 to 1
+  // const hue = ((1 - value) * 120).toString(10);
+  // return `hsl(${hue}, 100%, 50%)`;
+  if (days >= 15) {
+    return "#419C55"; // Green for days greater than 15
+  } else if (days >= 1 && days < 15) {
+    return "#EBB938"; // Yellow for days between 1 and 15
+  } else if (days === 0) {
+    return "#BE4444"; // Red for 0 days
+  } else {
+    return "#BE4444"; // Default color (you can change this to another color if needed)
+  }
 };
