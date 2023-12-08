0f7b5e9f3b2da0e0eacb0a545e30ebdf4833feae
Sandeep Singh Sikarwar <29565705+TheSikarwar@users.noreply.github.com>
2023-12-08 16:47:06 +0530

    add show more less and word limit

--- src/components/footer/innerfooter.jsx
+++ src/components/footer/innerfooter.jsx
@@ -265,7 +265,7 @@ const InnerFooter = () => {
                               // onClick={(e) => checkUserLoggedIn(e, USER_ROLES.vendor)}
                               disableGutters={true}
                             >
-                              {child.title}
+                              {child.title.length > 20 ? `${child.title.slice(0, 20)}...` : child.title}
                             </ListItemButton>
                           </ListItem>
                         );
@@ -334,7 +334,7 @@ const InnerFooter = () => {
                               checkUserLoggedIn(e, USER_ROLES.employer)
                             }
                           >
-                            {child.title}
+                            {child.title.length > 20 ? `${child.title.slice(0, 20)}...` : child.title}
                           </ListItemButton>
                         </ListItem>
                       );
