b2397301fbcfdb553d0fb70650e6d213cf33c936
AKASH SINGH TOMAR <99525697+akash481998@users.noreply.github.com>
2023-12-26 15:28:57 +0530

    slider issue fix

--- src/components/package/index.jsx
+++ src/components/package/index.jsx
@@ -13,7 +13,7 @@ export const Package = ({ packageData, handleBuyPackage }) => {
 
   const settings = {
     dots: true,
-    infinite: false,
+    infinite: true,
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 3,
