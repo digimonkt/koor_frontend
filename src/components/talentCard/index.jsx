d078b154bf3700e45de9180918be66a96800ef30
sanskar <sahup3296@gmail.com>
2024-02-15 15:08:59 +0530

    • Fix see more issue in talent search. ( Koor Web )

--- src/components/talentCard/index.jsx
+++ src/components/talentCard/index.jsx
@@ -29,7 +29,7 @@ function TalentCard({ talentDetails }) {
 
   const handleSeeMoreClick = () => {
     setNumLines((prevNumLines) =>
-      prevNumLines === 3 ? talentDetails.length : 3
+      prevNumLines === 3 ? talentDetails.description.length : 3,
     );
   };
   const textWrapperStyle = {
@@ -49,7 +49,7 @@ function TalentCard({ talentDetails }) {
         urlcat("/employer/chat", {
           conversion: conversationId,
           userId: talentDetails?.id,
-        })
+        }),
       );
     }
   };
