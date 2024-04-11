691f43f159b42b096556fe08b602f2491409c035
sanskar <sahup3296@gmail.com>
2024-04-11 18:46:37 +0530

    •    Fix applicants skeleton loading. ( Koor App and Dev )

--- src/components/applicantCard/skeletonLoading.jsx
+++ src/components/applicantCard/skeletonLoading.jsx
@@ -1,76 +1,40 @@
-import { Divider } from "@mui/material";
-import { Stack } from "@mui/system";
 import React from "react";
-import Skeleton from "react-loading-skeleton";
+import { Stack, Skeleton } from "@mui/material";
 
 const ApplicantCardSkeletonLoading = ({ sx }) => {
   return (
     <Stack
-      direction={{ xs: "column", lg: "row" }}
-      spacing={{ xs: "2", lg: "2" }}
-      alignItems={{ xs: "start", lg: "center" }}
-      justifyContent={{ xs: "center", lg: "space-between" }}
+      spacing={2}
+      justifyContent={{ lg: "space-between" }}
       className="border-recent"
+      sx={{ padding: 2 }}
+      direction="row"
     >
-      <Stack direction="row" spacing={2} alignItems="center">
-        {/* profile pic */}
-        <Skeleton circle style={{ width: "70px", height: "70px" }} />
-
-        <div className="recent-content">
-          <Stack
-            direction="row"
-            divider={<Divider orientation="vertical" flexItem />}
-            spacing={2}
-            flexWrap="wrap"
-            alignItems="center"
-            sx={{ mb: 1, ...sx }}
-          >
-            <h4>
-              <Skeleton width={720} />
-            </h4>
-          </Stack>
-          <Stack
-            direction="row"
-            spacing={2}
-            alignItems="center"
-            sx={{ mb: 1, ...sx }}
-            className="meets_div"
-          >
-            <div>
-              <span className="meets">
-                <Skeleton width={207} style={{ lineHeight: "9px" }} />
-              </span>
-            </div>
-            <div style={{ display: "flex" }}>
-              <Skeleton
-                height={30}
-                width={100}
-                style={{ borderRadius: "20px" }}
-                className="chiplabel"
-              />
-              <Skeleton
-                height={30}
-                width={100}
-                style={{ borderRadius: "20px" }}
-                className="chiplabel"
-              />
-              <Skeleton
-                height={30}
-                width={100}
-                style={{ borderRadius: "20px" }}
-                className="chiplabel"
-              />
-            </div>
-          </Stack>
-          <div className="recent-descrition">
-            <p>
-              <Skeleton width={930} count={2} style={{ lineHeight: "9px" }} />
-            </p>
-          </div>
-        </div>
-      </Stack>
-      <Stack direction="row" spacing={0} className="edit-button">
-        <Skeleton height={30} width={500} />
+      <Skeleton variant="circular" sx={{ width: 100, height: 100 }} />
+      <Stack direction="column" width="100%">
+        <Skeleton sx={{ width: "100%", height: 30 }} />
+        <Skeleton
+          sx={{
+            width: { xs: "50%", sm: "30%", lg: "10%" },
+            height: 20,
+            borderRadius: "20px",
+          }}
+        />
+        <Skeleton
+          sx={{
+            width: { xs: "50%", sm: "30%", lg: "10%" },
+            height: 20,
+            borderRadius: "20px",
+          }}
+        />
+        <Skeleton
+          sx={{
+            width: { xs: "50%", sm: "30%", lg: "10%" },
+            height: 20,
+            borderRadius: "20px",
+          }}
+        />
+        <Skeleton height={20} width="100%" />
       </Stack>
     </Stack>
   );
